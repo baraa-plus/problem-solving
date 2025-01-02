a5efc73e5ee18fc42bfb3f054a4396a3b3613647
baraa-plus <baraa.jaber.dev@gmail.com>
2025-01-02 18:53:10 +0300

    Create Range Sum of BST.py

---
+++ LeetCode/Range Sum of BST.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Method - 1 (Depth First Search):
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root == None:
+            return 0
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+        return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+# Method - 2 (Breadth First Search):
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        result = 0
+        bfs = []
+        bfs.append(root)
+        while (bfs):
+            r = bfs.pop()
+            if (r):
+                bfs.append(r.left)
+                bfs.append(r.right)
+                if (r.val >= low and r.val <= high):
+                    result += r.val
+        return result
