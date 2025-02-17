8c5bd5d6c6e277351491acb40f5e492e454d7df8
baraa-plus <baraa.jaber.dev@gmail.com>
2025-02-17 06:27:21 -0800

    Update Sum of Variable Length Subarrays.py

--- LeetCode/Sum of Variable Length Subarrays.py
+++ LeetCode/Sum of Variable Length Subarrays.py
@@ -1,3 +1,4 @@
+# Method - 1 (Brute Force):
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
         result = 0
@@ -6,4 +7,19 @@ class Solution:
             start = max(0, i - nums[i])
             for j in range(start, i + 1):
                 result += nums[j]
+        return result
+
+# Method - 2 (Prefix Sum):
+class Solution:
+    def subarraySum(self, nums) -> int:
+        n = len(nums)
+        pref = [0] * (n + 1)
+        for i in range(1, n + 1):
+            pref[i] = nums[i - 1] + pref[i - 1]
+
+        result = 0
+        for i in range(n):
+            start = max(0, i - nums[i])
+            result += pref[i + 1] - pref[start]
+        
         return result
