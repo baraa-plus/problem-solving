e8a1ac932e3b37ee00a5f1e8690edf07bcee42a0
baraa-plus <baraa.jaber.dev@gmail.com>
2025-04-25 15:36:00 +0300

    Create Minimum Operations to Make Array Values Equal to K.py

---
+++ LeetCode/Minimum Operations to Make Array Values Equal to K.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        result = 0
+        seen = set()
+
+        for num in nums:
+            if num < k:
+                return -1
+            elif num > k and num not in seen:
+                result += 1
+                seen.add(num)
+        
+        return result
+        
