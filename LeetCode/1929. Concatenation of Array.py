638ce2bd6e4bb1864cbcbfa47005286f81abea67
baraa-plus <baraa.jaber.dev@gmail.com>
2025-05-03 17:23:23 +0300

    Update 1929. Concatenation of Array.py

--- LeetCode/1929. Concatenation of Array.py
+++ LeetCode/1929. Concatenation of Array.py
@@ -1,6 +1,10 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        result = nums
-        for i in range(len(nums)):
-            result.append(nums[i])
+        n = len(nums)
+        result = [0] * n * 2
+
+        for i in range(n):
+            result[i] = nums[i]
+            result[i + n] = nums[i]
+        
         return result
